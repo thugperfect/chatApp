4746161ad01aed9df9bd896874d17b231e5e8acb
thugperfect <meenamani9444@gmail.com>
2023-09-02 06:49:31 +0530

    find chat page

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,6 +1,15 @@
 const Dashboard = ()=>{
     return(
-        <h1 className="class">Dashboard</h1>
+        <div className="text-gray-400 w-full  min-h-[100vh] flex flex-col justify-center items-center">
+            <div className="container w-[400px] h-[300px] outline outline-1 outline-slate-800 bg-black rounded-lg flex flex-col justify-evenly">
+                <div className="text-4xl w-full flex justify-center items-center">ChatApp</div>
+                <div className="w-full flex flex-col justify-center items-center gap-2">
+                    <input className=" w-[250px] rounded-md outline-none px-5 h-[30px]" type="text" name="chatname" id="chatname" placeholder="Enter Chat Name..."/>
+                    <button className="bg-slate-800 w-[250px] rounded-md px-5 h-[30px]" type="submit" >Create Chat</button>
+                </div>
+             
+            </div>
+        </div>
     )
 }
 export default Dashboard
