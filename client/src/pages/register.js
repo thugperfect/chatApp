a78901f7391b63146e2c06fe81db82205695e122
thugperfect <meenamani9444@gmail.com>
2023-09-02 10:59:06 +0530

    register complete

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -28,10 +28,16 @@ const Register = ()=>{
 console.log(result);
     return(
         <div className="text-gray-400 w-full  min-h-[100vh] flex flex-col justify-center items-center">
+            {
+                (result === "Register Success")?
+                <div className="bg-green-500 w-[400px] mb-2 flex justify-center items-center text-white rounded-lg">{result}</div>
+                : <div className="bg-red-500 w-[400px] mb-2 flex justify-center items-center text-white rounded-lg">{result}</div>
+            } 
+           
         <div className="container w-[400px] h-[500px] outline outline-1 outline-slate-800 bg-black rounded-lg flex flex-col justify-evenly">
             <div className="text-4xl w-full flex justify-center items-center">ChatApp</div>
             <div className="w-full flex flex-col justify-center items-center gap-2">
-            <input className=" w-[250px] rounded-md outline-none px-5 h-[30px]" ref={nameRef} type="text" name="name" id="name" placeholder="Enter your Name..."/>
+                <input className=" w-[250px] rounded-md outline-none px-5 h-[30px]" ref={nameRef} type="text" name="name" id="name" placeholder="Enter your Name..."/>
                 <input className=" w-[250px] rounded-md outline-none px-5 h-[30px]" ref={unameRef} type="text" name="username" id="username" placeholder="Enter your Username..."/>
                 <input className=" w-[250px] rounded-md outline-none px-5 h-[30px]" ref={emailRef} type="text" name="email" id="email" placeholder="Enter your Email..."/>
                 <input className=" w-[250px] rounded-md outline-none px-5 h-[30px]" ref={passwdRef} type="text" name="password" id="password" placeholder="Enter your Password..."/>
